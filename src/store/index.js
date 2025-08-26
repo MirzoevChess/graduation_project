192f6b3417fc979fbce6e73042535174f6cf2098
Ganna <19780101i.ua@gmail.com>
2025-08-26 16:16:07 +0200

    creat discount

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./features/categoriesSlice";
+import discountReducer from "./features/discountSlice"
 
 const store = configureStore({
   reducer: {
     categories: categoriesReducer,
+    discount: discountReducer,
   },
 });
 
