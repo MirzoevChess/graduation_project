940ecd4d56a5c2607d2e961da1488b7451e02ae7
Elmir <citruscentr@gmailcom>
2025-08-28 00:35:25 +0200

    store/index.js fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./features/categoriesSlice";
+import discountReducer from "./features/discountSlice"
 import { productsApi } from "./features/productsAPI";
 
 const store = configureStore({
